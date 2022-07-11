dc8a5f4b126059d0c6b17d213f4723ea95e754a4
Amaan <amaanmas1@gmail.com>
2022-07-11 20:19:27 +0530

    added files

---
+++ src/components/Home.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled from "styled-components"
+import Section from './Section'
+
+function Home() {
+
+    return (
+        <Container>
+            <div id="Model S">
+                <Section
+                    title="Model S"
+                    description="Order Online for Touchless Delivery"
+                    backgroundImg="model-s.jpg"
+                    leftBtnText="Custom Order"
+                    rightBtnText="Existing Inventory"
+                />
+            </div>
+            <div id="Model Y">
+                <Section
+                    title="Model Y"
+                    description="Order Online for Touchless Delivery"
+                    backgroundImg="model-y.jpg"
+                    leftBtnText="Custom Order"
+                    rightBtnText="Existing Inventory"
+                />
+            </div>
+            <div id="Model 3">
+                <Section
+                    title="Model 3"
+                    description="Order Online for Touchless Delivery"
+                    backgroundImg="model-3.jpg"
+                    leftBtnText="Custom Order"
+                    rightBtnText="Existing Inventory"
+                />
+            </div>
+            <div id="Model X">
+                <Section
+                    title="Model X"
+                    description="Order Online for Touchless Delivery"
+                    backgroundImg="model-x.jpg"
+                    leftBtnText="Custom Order"
+                    rightBtnText="Existing Inventory"
+                />
+            </div>
+            <div id="Solar Panels">
+                <Section
+                    title="Lowest Cost Solar Panels in America"
+                    description="Money-back guarantee"
+                    backgroundImg="solar-panel.jpg"
+                    leftBtnText="Order now"
+                    rightBtnText="Learn more"
+                />
+            </div>
+            <div id="Solar Roof">
+                <Section
+                    title="Solar for New Roofs"
+                    description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+                    backgroundImg="solar-roof.jpg"
+                    leftBtnText="Order now"
+                    rightBtnText="Learn more"
+                />
+            </div>
+
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop now"
+            />
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
